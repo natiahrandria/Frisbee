60b4b1ae38db2059315051cfee73c35b768e7162
Natiah Mialy Marion Randrianasolo <natiah.randrianasolo@dawsoncollege.qc.ca>
2019-05-02 09:13:36 -0400

    first commit

---
+++ Code/Frisbee.java
@@ -0,0 +1,11 @@
+// Import packages
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+import java.util.Scanner;
+import org.math.plot.*;
+import org.math.plot.plotObjects.*;
+import java.util.Arrays;
+
+public class Frisbee 
+{
